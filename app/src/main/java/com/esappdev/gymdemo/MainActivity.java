7978002e0e4395a23c6c24f02f3bdaefbb7e0c33
Esapp <info.esapp@gmail.com>
2017-05-24 21:59:11 +0200

    Implemented Firebase libraries

--- app/src/main/java/com/esappdev/gymdemo/MainActivity.java
+++ app/src/main/java/com/esappdev/gymdemo/MainActivity.java
@@ -1,12 +1,14 @@
 package com.esappdev.gymdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        if (BuildConfig.DEBUG) {
+            mFirebaseAnalytics.setAnalyticsCollectionEnabled(false);
+        }
+
+        // Firebase remote config.
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
     }
 }
